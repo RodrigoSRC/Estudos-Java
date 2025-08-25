7f0512476cbafe2762dc667b61528d7096f2daf1
Rodrigo <clancoto2015@hotmail.com>
2025-08-25 20:16:42 -0300

    Add comments to OlaDevDojo class for better understanding

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,12 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Essa é uma classe de exemplo para o DevDojo.
+ */
 public class OlaDevDojo {
+    // Isso é um comentário de uma linha
+    /* Isso é um comentário de múltiplas linhas
+       que pode abranger várias linhas */
     public static void main(String[] args) {
         System.out.println("Hello DevDojo!");
     }
