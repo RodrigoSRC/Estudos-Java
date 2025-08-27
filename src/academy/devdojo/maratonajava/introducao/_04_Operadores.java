ac4646de873da419819805e9beaafd112ad8b6c1
Rodrigo <clancoto2015@hotmail.com>
2025-08-27 14:38:08 -0300

    Refactor _04_Operadores class to improve arithmetic and comparison examples

--- src/academy/devdojo/maratonajava/introducao/_04_Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/_04_Operadores.java
@@ -2,9 +2,26 @@ package academy.devdojo.maratonajava.introducao;
 
 public class _04_Operadores {
     public static void main(String[] args) {
+
+        // + - * /
         int numero1 = 10;
         double numero2 = 20;
-        System.out.println(numero2 / numero1);
         System.out.println(numero1 / numero2);
+
+        // %
+        int restoPar = 20 % 2;
+        System.out.println(restoPar);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualAVinte = 10 == 20.0;
+        boolean isDezDiferenteDeVinte = 10 != 20.0;
+
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualAVinte "+isDezIgualAVinte);
+        System.out.println("isDezDiferenteDeVinte "+isDezDiferenteDeVinte);
+
     }
 }
