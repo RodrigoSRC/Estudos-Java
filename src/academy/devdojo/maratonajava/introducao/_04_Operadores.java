1422ad2b345c3ea9f91ed9b95725d0eae465a75a
Rodrigo <clancoto2015@hotmail.com>
2025-08-27 14:14:32 -0300

    Add _04_Operadores class to demonstrate basic arithmetic operations

---
+++ src/academy/devdojo/maratonajava/introducao/_04_Operadores.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class _04_Operadores {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        double numero2 = 20;
+        System.out.println(numero2 / numero1);
+        System.out.println(numero1 / numero2);
+    }
+}
