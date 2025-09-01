9d75b2f5f0e45cd2785179760c4b45a613af09e9
Rodrigo <clancoto2015@hotmail.com>
2025-09-01 16:50:05 -0300

    Add switch case examples for day and gender in _05_EstruturasCondicionais class

--- src/academy/devdojo/maratonajava/introducao/_05_EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/_05_EstruturasCondicionais.java
@@ -39,5 +39,60 @@ public class _05_EstruturasCondicionais {
         // }
 
         System.out.println(resultado);
+
+
+
+        byte dia = 5;
+        // char, int byte, short, enum, String
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+
+            case 2:
+                System.out.println("Segunda");
+                break;
+
+            case 3:
+                System.out.println("Terça");
+                break;
+
+            case 4:
+                System.out.println("Quarta");
+                break;
+
+            case 5:
+                System.out.println("Quinta");
+                break;
+
+            case 6:
+                System.out.println("Sexta");
+                break;
+
+            case 7:
+                System.out.println("Sábado");
+                break;
+
+            default:
+                System.out.println("Dia inválido");
+                break;
+        }
+
+
+        char sexo = 'M';
+
+        switch (sexo) {
+            case 'M':
+                System.out.println("Masculino");
+                break;
+
+            case 'F':
+                System.out.println("Feminino");
+                break;
+
+            default:
+                System.out.println("Só existem 2 gêneros e só Cristo salva");
+                break;
+        }
     }
 }
