4f06a6f18136cba83854199a7c7de6ed481c206a
Rodrigo <clancoto2015@hotmail.com>
2025-09-02 17:18:27 -0300

    Add implementation of _06_EstruturasDeRepeticao04 class to calculate and print installment values for a total amount

---
+++ src/academy/devdojo/maratonajava/introducao/_06_EstruturasDeRepeticao04.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class _06_EstruturasDeRepeticao04 {
+    public static void main(String[] args) {
+        // descubra em quantas vezes ele pode ser parcelado
+        // condição valor da parcela maior ou igual a mil
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela >= 1000) {
+                System.out.println("Valor da parcela: " + valorParcela);
+            } else {
+                break;
+            }
+        }
+    }
+}
