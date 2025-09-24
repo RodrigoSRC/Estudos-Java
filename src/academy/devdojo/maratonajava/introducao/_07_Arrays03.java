8e86ff72ee80f648ca1fd63185da0f99dd4e0727
Rodrigo <clancoto2015@hotmail.com>
2025-09-23 21:30:40 -0300

    Refactor _07_Arrays03 class to enhance array initialization and iteration examples

--- src/academy/devdojo/maratonajava/introducao/_07_Arrays03.java
+++ src/academy/devdojo/maratonajava/introducao/_07_Arrays03.java
@@ -12,5 +12,17 @@ public class _07_Arrays03 {
         }
 
         nomes = new String[4];
+
+        int[] numeros = new int[3];
+        int[] numeros2 = { 1, 2, 3, 4, 5 };
+        int[] numeros3 = new int[] { 5, 4, 3, 2, 1 };
+
+        // for (int i = 0; i < numeros2.length; i++) {
+        // System.out.println(numeros2[i]);
+        // }
+
+        for (int num : numeros3) {
+            System.out.println(num);
+        }
     }
 }
