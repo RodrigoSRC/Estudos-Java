8c5df8971d7465fc1684e0ca53d2861e3afee131
Rodrigo <clancoto2015@hotmail.com>
2026-05-27 21:42:31 -0300

    Add  _07_Arrays03 class to optimize array manipulation examples and improve iteration clarity

---
+++ src/academy/devdojo/maratonajava/introducao/_08_ArraysMultidimensionais03.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class _08_ArraysMultidimensionais03 {
+    public static void main(String[] args) {
+    
+    int[] array = {1, 2, 3};
+    int[][] arrayInt = new int[3][];
+
+    arrayInt[0] = new int[2];
+    arrayInt[1] = array;
+    arrayInt[2] = new int[6]{4, 5, 6, 7, 8, 9};
+
+    int[][] arrayInt2 = {{0, 0}, {1, 2, 3}, {4, 5, 6, 7, 8, 9}};
+
+    for(int[] arrBase: arrayInt) {
+        System.out.println("\n-----");
+        for(int num: arrBase) {
+            System.out.print(num + " ");
+        }
+    }
+    }
+}
