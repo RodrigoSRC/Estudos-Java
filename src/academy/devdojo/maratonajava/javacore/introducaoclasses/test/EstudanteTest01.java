cd51286f3d44d152e01cd307d3a52ae58efaa330
Rodrigo <clancoto2015@hotmail.com>
2026-05-29 09:05:01 -0300

    Remove EstudanteTest class as it is no longer needed for testing Estudante functionality

--- src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
 
 import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
 
-public class EstudanteTest {
+public class EstudanteTest01 {
     public static void main(String[] args) {
         Estudante estudante = new Estudante();
 
