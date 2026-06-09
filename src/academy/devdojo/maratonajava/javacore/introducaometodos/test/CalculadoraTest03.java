2134501c00bdfc69b72298ddbfcc85f316a46d0c
Rodrigo <clancoto2015@hotmail.com>
2026-06-09 11:19:55 -0300

    Update CalculadoraTest03 to test divideDoisNumeros method with valid inputs and print the result, improving test coverage and output clarity.

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/test/CalculadoraTest03.java
@@ -5,7 +5,8 @@ import academy.devdojo.maratonajava.javacore.introducaometodos.dominio.Calculado
 public class CalculadoraTest03 {
 public static void main(String[] args) {
     Calculadora calculadora = new Calculadora();
-    calculadora.divideDoisNumeros(10, 0);
+    double resultado = calculadora.divideDoisNumeros(20, 2);
+    System.out.println(resultado);
     System.out.println("Finalizando o metodo divideDoisNumeros da calculadoraTest03");
 }
 }
