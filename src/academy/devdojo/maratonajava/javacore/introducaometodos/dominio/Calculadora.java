58a0cab4e852905663fc91ded848616e436f0e7a
Rodrigo <clancoto2015@hotmail.com>
2026-06-09 15:33:43 -0300

    Refactor divideDoisNumeros method in Calculadora class to return a double result, and add a new divideDoisNumeros02 method for integer division with zero handling, enhancing functionality and flexibility.

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
@@ -10,10 +10,16 @@ public class Calculadora {
     public void multiplicaDoisNumeros(int num1, int num2) {
         System.out.println(num1 * num2);
     }
-    public int divideDoisNumeros(double num1, double num2) {
+    public double divideDoisNumeros(double num1, double num2) {
         if (num2 == 0) {
             return 0;
         }
-        return (int) (num1 / num2);
+        return num1 / num2;
+    }
+    public int divideDoisNumeros02(double num1, double num2) {
+        if (num2 != 0) {
+            return (int) (num1 / num2);
+        }
+        return 0;
     }
 }
