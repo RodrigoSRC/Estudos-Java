db11f96c3b081d3005c83de93e14812df8b5e063
Rodrigo <clancoto2015@hotmail.com>
2026-06-09 15:38:34 -0300

    Comment out the divideDoisNumeros02 method in Calculadora class to improve code clarity and maintainability, while retaining the original functionality for future reference.

--- src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Calculadora.java
@@ -16,10 +16,11 @@ public class Calculadora {
         }
         return num1 / num2;
     }
-    public int divideDoisNumeros02(double num1, double num2) {
-        if (num2 != 0) {
-            return (int) (num1 / num2);
-        }
-        return 0;
-    }
+    // public void divideDoisNumeros02(double num1, double num2) {
+    //     if (num2 == 0) {
+    //         System.out.println("Não é possível dividir por zero");
+    //         return;
+    //     }
+    //     System.out.println(num1 / num2);
+    // }
 }
