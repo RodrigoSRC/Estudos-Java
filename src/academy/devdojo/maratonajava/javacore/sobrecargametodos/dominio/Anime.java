d5a94838e550046fbfd76c8e5a8a078dd8ac1471
Rodrigo <clancoto2015@hotmail.com>
2026-06-15 11:36:16 -0300

    Add genero field and init2 method to Anime class for enhanced initialization; update AnimeTest01 to utilize new method for setting anime properties, improving test coverage and functionality.

--- src/academy/devdojo/maratonajava/javacore/sobrecargametodos/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/sobrecargametodos/dominio/Anime.java
@@ -4,6 +4,7 @@ public class Anime {
     private String nome;
     private String tipo;
     private int episodios;
+    private String genero;
 
     public void init(String nome, String tipo, int episodios) {
         this.nome = nome;
@@ -11,11 +12,18 @@ public class Anime {
         this.episodios = episodios;
     }
 
+    public void init2(String nome, String tipo, int episodios, String genero) {
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
+
     public void imprime() {
         System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.episodios);
+        System.out.println(this.genero);
     }
+
 
     public void setNome(String nome) {
         this.nome = nome;
@@ -25,6 +33,13 @@ public class Anime {
     }
     public void setEpisodios(int episodios) {
         this.episodios = episodios;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getGenero() {
+        return genero;
     }
     public String getNome() {
         return nome;
--- src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
@@ -5,7 +5,8 @@ import academy.devdojo.maratonajava.javacore.sobrecargametodos.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
-        anime.init("Akudama Drive", "TV", 12);
+        // anime.init("Akudama Drive", "TV", 12);
+        anime.init2("Akudama Drive", "TV", 12, "Ação");
         anime.imprime();
     }
     
