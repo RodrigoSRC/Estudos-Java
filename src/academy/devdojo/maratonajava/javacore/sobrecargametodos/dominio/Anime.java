b8d764e605a3d2db85a9cf9aabf0039fd6d2de17
Rodrigo <clancoto2015@hotmail.com>
2026-06-15 09:26:58 -0300

    Enhance Anime class by adding nome field with corresponding getter and setter methods, and update imprime method to display the anime name. Modify AnimeTest01 to validate the new functionality, improving usability and test coverage.

--- src/academy/devdojo/maratonajava/javacore/sobrecargametodos/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/sobrecargametodos/dominio/Anime.java
@@ -1,19 +1,27 @@
 package academy.devdojo.maratonajava.javacore.sobrecargametodos.dominio;
 
 public class Anime {
+    private String nome;
     private String tipo;
     private int episodios;
 
     public void imprime() {
+        System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.episodios);
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
     public void setEpisodios(int episodios) {
         this.episodios = episodios;
+    }
+    public String getNome() {
+        return nome;
     }
     public String getTipo() {
         return tipo;
