d9793a641529ea09182fa3ec9b8d8954fde00ba5
Rodrigo <clancoto2015@hotmail.com>
2026-06-15 09:27:43 -0300

    Update AnimeTest01 to change anime name from "One Piece" to "Akudama Drive", ensuring accurate test validation for the Anime class functionality.

--- src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
@@ -5,9 +5,9 @@ import academy.devdojo.maratonajava.javacore.sobrecargametodos.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
+        anime.setNome("Akudama Drive");
         anime.setTipo("TV");
         anime.setEpisodios(12);
-        anime.setNome("One Piece");
         anime.imprime();
     }
     
