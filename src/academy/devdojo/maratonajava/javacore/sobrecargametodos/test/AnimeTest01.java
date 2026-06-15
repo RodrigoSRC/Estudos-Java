e9220c6ad24ad4af6e0fddc7c2f4348dd9cd1ef7
Rodrigo <clancoto2015@hotmail.com>
2026-06-15 11:24:07 -0300

    Refactor AnimeTest01 to remove redundant property setters by utilizing the new init method in the Anime class, improving code clarity and reducing boilerplate.

--- src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
@@ -6,9 +6,6 @@ public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
         anime.init("Akudama Drive", "TV", 12);
-        anime.setNome("Akudama Drive");
-        anime.setTipo("TV");
-        anime.setEpisodios(12);
         anime.imprime();
     }
     
