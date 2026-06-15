0b7d3e8aed1cbddd70924483a1e269f561fb80e5
Rodrigo <clancoto2015@hotmail.com>
2026-06-15 11:55:01 -0300

    Remove redundant anime initialization in AnimeTest01; update to utilize the new init method with additional parameters for improved test coverage and clarity.

--- src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/sobrecargametodos/test/AnimeTest01.java
@@ -5,7 +5,6 @@ import academy.devdojo.maratonajava.javacore.sobrecargametodos.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
-        anime.init("Akudama Drive", "TV", 12);
         anime.init("Akudama Drive 2", "TV", 12, "Ação");
         anime.imprime();
     }
