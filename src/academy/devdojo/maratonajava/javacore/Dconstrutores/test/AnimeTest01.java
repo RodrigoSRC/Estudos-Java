e35d96df1056c57361eaa41a6cc0ec66d3b431ad
Rodrigo <clancoto2015@hotmail.com>
2026-06-15 14:22:30 -0300

    Refactor Anime class constructor to consolidate initialization and add genero field; update AnimeTest01 to utilize the new constructor for improved clarity and test coverage.

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,9 +4,8 @@ import academy.devdojo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
-        anime.init("Akudama Drive 2", "TV", 12, "Ação");
-        anime.imprime();
+        Anime anime = new Anime("Monster", "TV", 12, "Ação");
+        anime.imprime();    
     }
     
 }
