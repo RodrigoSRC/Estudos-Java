9dae3a7eec9daa1cc7d57fe1dd2855518b21edef
Rodrigo <clancoto2015@hotmail.com>
2026-06-17 10:25:22 -0300

    Refactor Time class to improve jogadores field management and enhance imprime method for better team and player display, increasing functionality and clarity.

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/LeituraDoTecladoTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String next = entrada.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade = entrada.nextInt();
+        System.out.println("Digite M ou F para o sexo: ");
+        char sexo = entrada.next().charAt(0);
+        System.out.println("Nome: " + next + " Idade: " + idade + " Sexo: " + sexo);
+        entrada.close();
+    }
+}
