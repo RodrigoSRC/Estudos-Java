9516c7e0ff01fe9254e207b45c279422820f8e91
Rodrigo <clancoto2015@hotmail.com>
2026-06-27 14:15:54 -0300

    Refactor Funcionario and Pessoa classes to improve debugging output by adding static and instance initialization blocks. This change enhances visibility into the object creation process, aiding in understanding class behavior.

---
+++ src/academy/devdojo/maratonajava/javacore/Isobreescrita/dominio/Anime.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.Isobreescrita.dominio;
+
+public class Anime extends Object {
+    private String nome;
+
+    // @Override
+    // public String toString() {
+    // return "Anime: " + this.nome;
+    // // return "Anime{" +
+    // // "nome='" + nome + '\'' +
+    // // '}';
+    // }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Anime [nome=" + nome + "]";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
