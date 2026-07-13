76bd887980e108c74f33ca60de5d8358aaa7136c
Rodrigo <clancoto2015@hotmail.com>
2026-07-13 11:59:31 -0300

    Implement data retrieval methods in DataLoader interface and DataBaseLoader class, enhancing functionality for maximum data size retrieval. Update DataLoaderTest01 to validate new methods.

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+
+    }
+}
