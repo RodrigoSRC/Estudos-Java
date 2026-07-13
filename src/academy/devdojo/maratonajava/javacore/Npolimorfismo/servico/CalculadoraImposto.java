fc8ff507b1b9ebad905f90f0f03cfb04b3a56837
Rodrigo <clancoto2015@hotmail.com>
2026-07-13 14:15:27 -0300

    Refactor CalculadoraImposto to use a single method for calculating tax on different product types. Update ProdutoTest01 to reflect changes and include a new Televisao class for testing.

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,23 +1,14 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.servico;
 
-import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
-import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
 
 public class CalculadoraImposto {
-    public static void calcularImpostoComputador(Computador computador) {
-        System.out.println("Relatorio de imposto do computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Imposto do computador: " + computador.getNome());
-        System.out.println("Valor: " + computador.getValor());
-        System.out.println("Imposto: " + imposto);
 
-    }
-
-    public static void calcularImpostoTomate(Tomate tomate) {
-        System.out.println("Relatorio de imposto do tomate");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Imposto do tomate: " + tomate.getNome());
-        System.out.println("Valor: " + tomate.getValor());
+    public static void calcularImposto(Produto produto) {
+        System.out.println("Relatorio de imposto do produto");
+        double imposto = produto.calcularImposto();
+        System.out.println("Imposto do produto: " + produto.getNome());
+        System.out.println("Valor: " + produto.getValor());
         System.out.println("Imposto: " + imposto);
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -3,12 +3,15 @@ package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 
 public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("Ryzen 9", 10000);
         Tomate tomate = new Tomate("Tomate", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        Televisao televisao = new Televisao("Televisão 50\" ", 5000);
+        CalculadoraImposto.calcularImposto(computador);
+        CalculadoraImposto.calcularImposto(tomate);
+        CalculadoraImposto.calcularImposto(televisao);
     }
 }
