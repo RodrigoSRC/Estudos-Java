1534b5bdb89f1bd7259f5fdb9635d7f8b2e14677
Rodrigo <clancoto2015@hotmail.com>
2026-08-24 22:34:58 -0300

    Enhance CalculadoraImposto by adding alternative Tomate validity date retrieval code for future reference. This maintains existing functionality while improving clarity.

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTeste.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.repositorio.Repositorio;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioBancoDeDados;
+import java.util.List;
+import java.util.ArrayList;
+
+public class RepositorioTeste {
+    public static void main(String[] args) {
+        Repositorio repositorio = new RepositorioBancoDeDados();
+        repositorio.salvar();
+
+        List<String> list = new ArrayList<>(); // ou linkedList
+        list.add("João");
+        list.add("Maria");
+        list.add("Pedro");
+
+    }
+}
